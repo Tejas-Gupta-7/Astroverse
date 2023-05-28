fb3835a63d08aef848f353be682d43b476d7bdcf
Tejas Gupta <115937671+Tejas-Gupta-7@users.noreply.github.com>
2023-05-28 14:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+window.addEventListener("load", function() {
+  var loader = document.getElementById("loader");
+  var loaderText = document.getElementById("loader-text");
+  var preloader = document.getElementById("preloader");
+  // Delay in milliseconds before hiding the preloader (5 seconds in this example)
+  var delayInMilliseconds = 2500;
+
+  // Hide the preloader after the specified delay
+  setTimeout(function() {
+    loader.style.display = "none";
+    loaderText.style.display = "none";
+    preloader.style.display = "none";
+    hidden.style.display="initial";
+  }, delayInMilliseconds);
+   
+});
